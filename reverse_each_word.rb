e1ab427b867397597e89a16f4c14154c3249f270
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-26 19:03:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 
 def reverse_each_word(str)
-  str.each_char{|s|
-str[] +=[s," "]
-  }
-  new_str=str.collect { |e| e.reverse
+  arr= str.split
+  new_str=arr.collect { |e| e.reverse
 
  }
  new_str.join(" ")
